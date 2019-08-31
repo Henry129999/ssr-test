0aa451ed1f6e86640aa12eb76806f257eed6d6c3
huanghao04 <huanghao04@fangdd.com>
2019-08-31 20:09:32 +0800

    服务器端渲染的路由

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,19 @@
-const React = require('react');
+import React from 'react';
 const ReactDOMServer = require('react-dom/server');
+const { StaticRouter } = require('react-router-dom');
+import Routes from '../routes';
+
 const express = require('express');
 const app = express();
-import Home from '../client/containers/Home'
 const port = 3000;
-
-const content = ReactDOMServer.renderToString(<Home />);
 
 // 服务器请求静态文件，就是根目录public中无寻找
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send(`<html lang="utf-8"><header><title>ssr</title></header><body><div id="root">${content}</div></body><script src="index.js"></script></html>`));
+app.get('/', (req, res) => {
+  console.log('path', req.path);
+  const content = ReactDOMServer.renderToString(<StaticRouter path={req.path} context={{}}>{Routes}</StaticRouter>);
+  res.send(`<html lang="utf-8"><header><title>ssr</title></header><body><div id="root">${content}</div></body><script src="index.js"></script></html>`)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
