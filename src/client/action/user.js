f46ef6aba4cd143e9e39847ee7ab8d04a50ff19b
huanghao04 <huanghao04@fangdd.com>
2019-09-01 21:29:35 +0800

    axios instance axios区分环境

--- src/client/action/user.js
+++ src/client/action/user.js
@@ -1,4 +1,6 @@
-import axios from 'axios';
+import clientRequest from '../request';
+import serverRequest from '../../server/request';
+
 export const ADD_NAME = 'ADD_NAME';
 export const ADD_NEWS_LIST = 'ADD_NEWS_LIST';
 
@@ -19,15 +21,11 @@ const newsList = (list) => {
 export const getUserInfo = (server) => {
   // http://47.95.113.63/ssr/api/news.json?secret=PP87ANTIPIRATE
   // 区分环境
-  let url = '';
-  if (server) {
-    url= 'http://47.95.113.63/ssr/api/news.json?secret=PP87ANTIPIRATE';
-  } else {
-    url = '/api/news.json?secret=PP87ANTIPIRATE';
-  }
+  let request = '';
+  request = server ? serverRequest : clientRequest;
 
   return (dispatch) => {
-    return axios.get(url)
+    return request.get('/api/news.json?secret=PP87ANTIPIRATE')
       .then(res=> {
         if (res) {
           dispatch(newsList((res.data || {}).data || []));
